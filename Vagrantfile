3540dad9436605f464eaebfb35192a959907f02c
Aj Cerqueti <aj@cerqueti.co.uk>
2018-04-12 11:59:00 +0100

    Update box

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,19 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
+    # Hostname
     config.vm.hostname = "redis"
-    config.vm.box = "ubuntu/trusty32"
-    config.vm.network :forwarded_port, guest: 6379, host: 6379
+
+    # Box OS
+    config.vm.box = "ubuntu/xenial64"
+
+    # Enable ports
+    config.vm.network :forwarded_port, guest: 6379, host: 6379, host_ip: "127.0.0.1"
+
+    # Configure VirtualBox
+    config.vm.provider "virtualbox" do |machine|
+        machine.name = "redis"
+    end
+
     config.vm.provision :shell, :path => "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-apt-get update > /dev/null
+echo "Updating..."
+sudo apt-get update > /dev/null
 
-apt-get -y install make
+sudo apt-get -y install make build-essential
 
-mkdir /opt/redis
+sudo mkdir /opt/redis
 
 cd /opt/redis
 # Use latest stable
-wget -q http://download.redis.io/redis-stable.tar.gz
+sudo wget -q http://download.redis.io/redis-stable.tar.gz
 # Only update newer files
-tar -xz --keep-newer-files -f redis-stable.tar.gz
+sudo tar -xz --keep-newer-files -f redis-stable.tar.gz
 
 cd redis-stable
-make
-make install
-rm /etc/redis.conf
-mkdir -p /etc/redis
-mkdir /var/redis
-chmod -R 777 /var/redis
-useradd redis
+sudo make
+sudo make install
+sudo rm -rf /etc/redis.conf
+sudo mkdir -p /etc/redis
+sudo mkdir /var/redis
+sudo chmod -R 777 /var/redis
+sudo useradd redis
 
-cp -u /vagrant/redis.conf /etc/redis/6379.conf
-cp -u /vagrant/redis.init.d /etc/init.d/redis_6379
+sudo cp -u /vagrant/redis.conf /etc/redis/6379.conf
+sudo cp -u /vagrant/redis.init.d /etc/init.d/redis_6379
 
-update-rc.d redis_6379 defaults
+sudo update-rc.d redis_6379 defaults
 
-chmod a+x /etc/init.d/redis_6379
-/etc/init.d/redis_6379 start
+sudo chmod a+x /etc/init.d/redis_6379
+sudo /etc/init.d/redis_6379 start
